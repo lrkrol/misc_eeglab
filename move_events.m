4547d348fb8f3b0a2cdc9394ed000a941d1adec1
Laurens R Krol <lrkrol+github@gmail.com>
2019-03-07 18:17:34 +0100

    Allowed time in ms as argument

--- move_events.m
+++ move_events.m
@@ -1,24 +1,27 @@
-% [EEG, diffs] = move_events(EEG, moveevents, targetevents)
+% [EEG, diffs] = move_events(EEG, moveevents, target)
 %
-%       Moves indicated events forward in time to have the same 
-%       latency as the nearest following target event.
+%       Given continuous data, moves indicated events, either 
+%       - forward in time to have the same latency as the nearest
+%         following specified target event, or
+%       - a specified amount forward/backward in time.
 %
 %       Note that events are indicated as regular expressions and the
 %       script will move all matches. Use the OR operator to indicate
 %       multiple events, e.g. 'event1|event2|event3', or wildcards, e.g.
-%       'event*'.
-%
-%       Also note that this is currently not checking for epoch boundaries:
-%       events in epoched data could theoretically be moved into another
-%       epoch.
+%       'event.+'. '.*' indicates all events. Also make sure to use anchors
+%       where necessary: 'target' will match both 'target' and 'nontarget',
+%       because the start-of-line anchor was left out. '^target' will not
+%       match 'nontarget', but it will match 'targetasdf'. '^target$' will
+%       match only 'target'.
 %
 % In:
 %       EEG - EEGLAB dataset with event structure
 %       moveevents - regular expression representing the event types
 %                    (markers) of the events that are to be moved
-%       targetevents - regular expression representing the event types
-%                      (markers) of the events to which earlier events are
-%                      to be moved
+%       target - if numeric: time in ms to shift the indicated events.
+%                if string: regular expression representing the event types
+%                (markers) of the events to which earlier events are to be
+%                moved.
 %
 % Out:
 %       EEG - the updated EEGLAB dataset
@@ -32,7 +35,13 @@
 %       To move events 'A10', 'A15', and 'B10' forward to 'photo-onset'
 %       while ignoring 'B15':
 %       >> [EEG, diffs] = move_events(EEG, 'A1*|B10', 'photo-onset');
+%
+%       To move all events 150 ms forward in time:
+%       >> [EEG, ~] = move_events(EEG, '.*', 150);
 
+% 2019-03-06 lrk
+%   - Changed 'targetevents' to 'target', now also accepts numerical
+%     time to shift events a specified amount
 % 2019-02-04 First version
 
 % This program is free software: you can redistribute it and/or modify
@@ -49,7 +58,7 @@
 % along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-function [EEG, diffs] = move_events(EEG, moveevents, targetevents)
+function [EEG, diffs] = move_events(EEG, moveevents, target)
 
 % sorting events
 if isfield(EEG.event, 'epoch')
@@ -60,27 +69,45 @@ end
 
 diffs = [];
 misses = 0;
-for e = 1:length(EEG.event)
-    % finding indicated events
-    if regexp(EEG.event(e).type, moveevents)        
-        % finding earliest following target event
-        counter = 1;
-        while isempty(regexp(EEG.event(e + counter).type, targetevents, 'once'))
-            counter = counter + 1;
+
+if ischar(target)
+    for e = 1:length(EEG.event)
+        % finding indicated events
+        if regexp(EEG.event(e).type, moveevents)        
+            % finding earliest following target event
+            counter = 1;
+            while isempty(regexp(EEG.event(e + counter).type, target, 'once'))
+                counter = counter + 1;
+                if e + counter > length(EEG.event)
+                    warning('could not find target event for event number %d, %s', e, EEG.event(e).type);
+                    misses = misses + 1;
+                    break
+                end
+            end
+            
             if e + counter > length(EEG.event)
-                warning('could not find target event for event number %d, %s', e, EEG.event(e).type);
-                misses = misses + 1;
-                break
+                % no target event found; continuing on to next event
+                continue
+            else
+                % updating event latency
+                diffs = [diffs, EEG.event(e + counter).latency - EEG.event(e).latency];
+                EEG.event(e).latency = EEG.event(e + counter).latency;
             end
         end
-        
-        if e + counter > length(EEG.event)
-            % no target event found; continuing on to next event
-            continue
-        else
-            % updating event latency
-            diffs = [diffs, EEG.event(e + counter).latency - EEG.event(e).latency];
-            EEG.event(e).latency = EEG.event(e + counter).latency;
+    end
+elseif isnumeric(target)
+    % getting latency difference
+    latdiff = EEG.srate * target/1000;
+    for e = 1:length(EEG.event)
+        % finding indicated events
+        if regexp(EEG.event(e).type, moveevents)
+            if EEG.event(e).latency + latdiff <= EEG.pnts
+                diffs = [diffs, EEG.event(e).latency + latdiff - EEG.event(e).latency];
+                EEG.event(e).latency = EEG.event(e).latency + latdiff;
+            else
+                warning('cannot move event number %d, %s, out of range', e, EEG.event(e).type);
+                misses = misses + 1;
+            end
         end
     end
 end
